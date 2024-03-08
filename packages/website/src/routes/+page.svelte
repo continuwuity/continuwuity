28d62cd4141526b805f6d25e22ee56328e19cab1
Jade Ellis <jade@ellis.link>
2024-03-08 00:21:15 +0000

    Add meta

--- packages/website/src/routes/+page.svelte
+++ packages/website/src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
     import Hero from "$lib/Hero.svelte";
+    import SvelteSeo from "svelte-seo";
 </script>
-
+<SvelteSeo
+  title="JadedBlueEyes"
+  description="My website -- under construction for now!"
+  canonical="https://jade.ellis.link/"
+/>
 <Hero />
