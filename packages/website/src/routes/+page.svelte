d41626553fd64c06ba80a4682e935495e7888e20
Jade Ellis <jade@ellis.link>
2024-03-10 10:29:46 +0000

    Add maston and better meta

--- packages/website/src/routes/+page.svelte
+++ packages/website/src/routes/+page.svelte
@@ -3,8 +3,9 @@
     import SvelteSeo from "svelte-seo";
 </script>
 <SvelteSeo
-  title="JadedBlueEyes"
-  description="My website -- under construction for now!"
-  canonical="https://jade.ellis.link/"
+  title="Jade Ellis"
+  description="Student, Creative & Computer Scientist. See what I'm doing."
+  canonical="https://jade.ellis.link"
 />
 <Hero />
+<a rel="me" href="https://tech.lgbt/@JadedBlueEyes">Mastodon</a>
