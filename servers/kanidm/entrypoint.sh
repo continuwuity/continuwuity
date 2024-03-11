b9eb87d76d8897be40972ffeaa8bab1668fda1ef
Jade Ellis <jade@ellis.link>
2024-03-11 13:22:38 +0000

    Kanidm setup

---
+++ servers/kanidm/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f /data/certs/cert.pem ]; then
+    echo "Generating certs"
+    /sbin/kanidmd cert-generate -c /data/server.toml
+fi
+
+/sbin/kanidmd server -c /data/server.toml
