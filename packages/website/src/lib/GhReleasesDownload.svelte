3b3cabb1b67c645c0f69d56d953733878fcbedf6
Jade Ellis <jade@ellis.link>
2024-07-16 19:42:18 +0100

    Improve error handling in gh download

--- packages/website/src/lib/GhReleasesDownload.svelte
+++ packages/website/src/lib/GhReleasesDownload.svelte
@@ -2,31 +2,35 @@
     import type { Endpoints } from "@octokit/types";
 
     export let releaseData: Endpoints["GET /repos/{owner}/{repo}/releases/latest"]["response"]["data"];
-    import { browser } from '$app/environment';
+    import { browser } from "$app/environment";
     // console.log(releaseData);
 </script>
 
 <div class="release">
     {#if browser && navigator}
         {#if navigator.platform.startsWith("Win")}
-            {@const asset  =releaseData.assets.filter((a) => a.name.endsWith(".exe"))[0]}
+            {@const asset = (releaseData?.assets || []).filter((a) =>
+                a.name.endsWith(".exe"),
+            )[0]}
             {#if asset}
                 <a href={asset.browser_download_url}>Download for Windows</a>
             {/if}
         {:else if navigator.platform.startsWith("Mac")}
-        {@const asset  =releaseData.assets.filter((a) => a.name.endsWith(".dmg"))[0]}
-        {#if asset}
-            <a href={asset.browser_download_url}>Download for MacOS</a>
-        {/if}
-
+            {@const asset = (releaseData?.assets || []).filter((a) =>
+                a.name.endsWith(".dmg"),
+            )[0]}
+            {#if asset}
+                <a href={asset.browser_download_url}>Download for MacOS</a>
+            {/if}
         {:else if navigator.platform.startsWith("Linux") && navigator.platform.includes("x86_64")}
-
-        {@const asset  =releaseData.assets.filter((a) => a.name.endsWith(".AppImage"))[0]}
-        {#if asset}
-            <a href={asset.browser_download_url}>Download AppImage</a>
+            {@const asset = (releaseData?.assets || []).filter((a) =>
+                a.name.endsWith(".AppImage"),
+            )[0]}
+            {#if asset}
+                <a href={asset.browser_download_url}>Download AppImage</a>
+            {/if}
         {/if}
-        {/if}
-    <!-- {:else} -->
+        <!-- {:else} -->
     {/if}
     <p>Latest release: <a href={releaseData.html_url}>{releaseData.name}</a></p>
 </div>
