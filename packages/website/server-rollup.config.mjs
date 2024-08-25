2b59fa45a1b16e9663b10e48c17ac594b2226a0d
Jade Ellis <jade@ellis.link>
2024-08-25 02:46:26 +0100

    Upload server sourcemaps

--- packages/website/server-rollup.config.mjs
+++ packages/website/server-rollup.config.mjs
@@ -2,14 +2,22 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
+import { sentryRollupPlugin } from "@sentry/rollup-plugin";
 
 export default {
     input: 'build/index.js',
     output: {
         dir: "output",
-        format: 'esm'
+        format: 'esm',
+        sourcemap: true,
     },
     // external: id => id.startsWith("@resvg/resvg-js-"),
     external: ["@resvg/resvg-js"],
-    plugins: [nodeResolve(), json(), commonjs()]
+    plugins: [
+        nodeResolve(), json(), commonjs(),
+        sentryRollupPlugin({
+            org: "jade-ellis",
+            project: "jade-website-sveltekit",
+        }),
+    ]
 };
