ea72673d320ed459ffbfa3f992dce0921ebbace7
Jade Ellis <jade@ellis.link>
2024-11-25 01:55:13 +0000

    mautrix autoconfig

---
+++ packages/website/src/routes/.well-known/matrix/mautrix/+server.ts
@@ -0,0 +1,13 @@
+export async function GET({ url }) {
+    const res = new Response(JSON.stringify({
+        "fi.mau.bridges": [
+            "https://mautrix-discord.ellis.link",
+            "https://mautrix-gmessages.ellis.link",
+            "https://mautrix-meta.ellis.link",
+            "https://mautrix-signal.ellis.link",
+            "https://mautrix-slack.ellis.link",
+            "https://mautrix-whatsapp.ellis.link"
+        ]
+    }), { headers: { "content-type": "application/jrd+json" }, status: 200 })
+    return res;
+}
