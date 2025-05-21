a33179005852eec57c6ee512b865223f4a3b6d48
Jade Ellis <jade@ellis.link>
2025-05-21 22:24:33 +0100

    feat: Add admin command to delete sync tokens from a room

--- src/admin/room/mod.rs
+++ src/admin/room/mod.rs
@@ -6,7 +6,7 @@ mod moderation;
 
 use clap::Subcommand;
 use conduwuit::Result;
-use ruma::OwnedRoomId;
+use ruma::{OwnedRoomId, OwnedRoomOrAliasId};
 
 use self::{
 	alias::RoomAliasCommand, directory::RoomDirectoryCommand, info::RoomInfoCommand,
@@ -55,5 +55,12 @@ pub(super) enum RoomCommand {
 	/// - Check if we know about a room
 	Exists {
 		room_id: OwnedRoomId,
+	},
+
+	/// - Delete all sync tokens for a room
+	PurgeSyncTokens {
+		/// Room ID or alias to purge sync tokens for
+		#[arg(value_parser)]
+		room: OwnedRoomOrAliasId,
 	},
 }
