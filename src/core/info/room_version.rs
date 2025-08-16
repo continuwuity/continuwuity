54acd07555abeff1890a02d6ece66b6c97abdc02
nexy7574 <git@nexy7574.co.uk>
2025-08-16 16:22:24 +0100

    fix: Drop fake room v2 support

--- src/core/info/room_version.rs
+++ src/core/info/room_version.rs
@@ -18,7 +18,7 @@ pub const STABLE_ROOM_VERSIONS: &[RoomVersionId] = &[
 
 /// Experimental, partially supported room versions
 pub const UNSTABLE_ROOM_VERSIONS: &[RoomVersionId] =
-	&[RoomVersionId::V2, RoomVersionId::V3, RoomVersionId::V4, RoomVersionId::V5];
+	&[RoomVersionId::V3, RoomVersionId::V4, RoomVersionId::V5];
 
 type RoomVersion = (RoomVersionId, RoomVersionStability);
 
