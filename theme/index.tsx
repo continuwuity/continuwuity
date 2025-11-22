3bf3c24d22de5722c06740b6fe2a43b4bae52a71
Jade Ellis <jade@ellis.link>
2025-11-22 22:41:53 +0000

    docs: Update homepage layout

--- theme/index.tsx
+++ theme/index.tsx
@@ -1,4 +1,29 @@
+import { HomeLayout as BasicHomeLayout, DocContent } from "@rspress/core/theme";
 
-export * from '@rspress/core/theme';
+import { useFrontmatter } from '@rspress/core/runtime';
+interface HomeLayoutProps {
+    components?: Record<string, React.FC>;
+}
 
-import './index.css';
+function HomeLayout(props: HomeLayoutProps) {
+    console.log(props)
+
+    const { frontmatter } = useFrontmatter();
+
+    return (
+        <BasicHomeLayout
+            afterFeatures={
+                (frontmatter.doc) ?
+                <main className="rp-doc-layout__doc-container">
+                    <div className="rp-doc rspress-doc">
+                        <DocContent components={props.components} />
+                    </div>
+                </main>
+                : <></>
+            }
+        />
+    );
+}
+export { HomeLayout };
+export * from "@rspress/core/theme";
+import "./index.css";
