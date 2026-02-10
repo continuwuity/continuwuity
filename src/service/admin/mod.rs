d1e1ee6156537e9eaaea1621d2d36eb917aa09a9
chri-k <chri.k@icloud.com>
2026-02-10 13:56:22 +0000

    fix: always treat server_user as an admin

--- src/service/admin/mod.rs
+++ src/service/admin/mod.rs
@@ -406,6 +406,10 @@ impl Service {
 
 	/// Checks whether a given user is an admin of this server
 	pub async fn user_is_admin(&self, user_id: &UserId) -> bool {
+		if self.services.globals.server_user == user_id {
+			return true
+		}
+
 		if self
 			.services
 			.server
