26aac1408e7c69e00b88a911e0122ccd249242ce
Jade Ellis <jade@ellis.link>
2026-02-15 14:33:27 +0000

    fix: Correct user agent changes
    Correct the domain
    Remove "embed" in the UA because the
    global UA was modified, rather than
    just the one for preview requests

--- src/core/info/version.rs
+++ src/core/info/version.rs
@@ -8,10 +8,11 @@
 use std::sync::OnceLock;
 
 static BRANDING: &str = "continuwuity";
-static WEBSITE: &str = "https://continuwuity.com";
+static WEBSITE: &str = "https://continuwuity.org";
 static SEMANTIC: &str = env!("CARGO_PKG_VERSION");
 
 static VERSION: OnceLock<String> = OnceLock::new();
+static VERSION_UA: OnceLock<String> = OnceLock::new();
 static USER_AGENT: OnceLock<String> = OnceLock::new();
 
 #[inline]
@@ -20,17 +21,17 @@ pub fn name() -> &'static str { BRANDING }
 
 #[inline]
 pub fn version() -> &'static str { VERSION.get_or_init(init_version) }
+#[inline]
+pub fn version_ua() -> &'static str { VERSION_UA.get_or_init(init_version_ua) }
 
 #[inline]
 pub fn user_agent() -> &'static str { USER_AGENT.get_or_init(init_user_agent) }
 
-fn init_user_agent() -> String {
-	format!(
-		"{}/{SEMANTIC}{} (embedbot; +{WEBSITE})",
-		name(),
-		conduwuit_build_metadata::version_tag()
-			.map_or_else(String::new, |extra| format!("+{extra}"))
-	)
+fn init_user_agent() -> String { format!("{}/{} (bot; +{WEBSITE})", name(), version_ua()) }
+
+fn init_version_ua() -> String {
+	conduwuit_build_metadata::version_tag()
+		.map_or_else(|| SEMANTIC.to_owned(), |extra| format!("{SEMANTIC}+{extra}"))
 }
 
 fn init_version() -> String {
