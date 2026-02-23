c3f6c73ac402deca2cc5715c5e4d8c9edace44d8
timedout <git@nexy7574.co.uk>
2026-02-23 20:58:22 +0000

    feat: Add compile flag to instantly drop mutex for A/B testing

--- src/api/client/sync/v3/joined.rs
+++ src/api/client/sync/v3/joined.rs
@@ -66,6 +66,8 @@ pub(super) async fn load_joined_room(
 	*/
 
 	let insert_lock = services.rooms.timeline.mutex_insert.lock(room_id).await;
+	#[cfg(feature = "sync-drop-instant")]
+	drop(insert_lock);
 	let (
 		account_data,
 		ephemeral,
@@ -83,6 +85,7 @@ pub(super) async fn load_joined_room(
 	)
 	.boxed()
 	.await?;
+	#[cfg(not(feature = "sync-drop-instant"))]
 	drop(insert_lock);
 
 	if !timeline.is_empty() || !state_events.is_empty() {
