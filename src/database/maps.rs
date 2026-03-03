bc0c4d8f4cc9b415b3aef20c7a35bdbaea4aa4c3
Ginger <ginger@gingershaped.computer>
2026-03-03 10:36:25 -0500

    feat: Add a new service for handling password resets

--- src/database/maps.rs
+++ src/database/maps.rs
@@ -113,6 +113,10 @@ pub(super) static MAPS: &[Descriptor] = &[
 		..descriptor::RANDOM_SMALL
 	},
 	Descriptor {
+		name: "passwordresettoken_userid",
+		..descriptor::RANDOM_SMALL
+	},
+	Descriptor {
 		name: "pduid_pdu",
 		cache_disp: CacheDisp::SharedWith("eventid_outlierpdu"),
 		key_size_hint: Some(16),
