ec5f50c68ec850d5f53fda6ef8afbd6fe8b417fd
Henry-Hiles <henry@henryhiles.com>
2026-03-30 21:23:00 -0400

    chore: rewrite continuwuity build

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -1,16 +1,53 @@
+{ inputs, ... }:
 {
-  imports = [
-    ./continuwuity
-    ./rust.nix
-    ./uwulib
-  ];
-
   perSystem =
-    { self', pkgs, ... }:
     {
-      packages = {
-        default = self'.packages.continuwuity-default-bin;
-        rocksdb = pkgs.callPackage ./rocksdb.nix { };
-      };
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      packages =
+        let
+          crane = self'.packages.crane;
+          src =
+            let
+              # see https://crane.dev/API.html#cranelibfiltercargosources
+              # we need to keep the `web` directory which would be filtered out by the regular source filtering function
+              # https://crane.dev/API.html#cranelibcleancargosource
+              isWebTemplate = path: _type: builtins.match ".*(src/(web|service)|docs).*" path != null;
+              isRust = crane.filterCargoSources;
+              isNix = path: _type: builtins.match ".+/nix.*" path != null;
+              webOrRustNotNix = p: t: !(isNix p t) && (isWebTemplate p t || isRust p t);
+            in
+            lib.cleanSourceWith {
+              src = inputs.self;
+              filter = webOrRustNotNix;
+              name = "source";
+            };
+
+          common = {
+            inherit src;
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.liburing ];
+            env.LIBCLANG_PATH = lib.makeLibraryPath [ pkgs.llvmPackages.libclang.lib ];
+          };
+
+          cargoArtifacts = crane.buildDepsOnly common;
+
+          continuwuity = crane.buildPackage (
+            common
+            // {
+              inherit cargoArtifacts;
+              doCheck = false;
+              env.LIBCLANG_PATH = lib.makeLibraryPath [ pkgs.llvmPackages.libclang.lib ];
+            }
+          );
+        in
+        {
+          default = continuwuity;
+          rocksdb = pkgs.callPackage ./rocksdb.nix { };
+        };
     };
 }
