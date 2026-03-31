c278663f6596ec9b9ddedf71af5695e8d055f63f
Henry-Hiles <henry@henryhiles.com>
2026-03-31 17:39:17 -0400

    fix: devshell fixes
    Co-authored-by: kraem <
    me@kraem.xyz>

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -11,13 +11,24 @@
       # basic nix shell containing all things necessary to build continuwuity in all flavors manually (on x86_64-linux)
       devShells.default = craneLib.devShell {
         packages = [
-          pkgs.nodejs
-          pkgs.pkg-config
-          pkgs.liburing
-          pkgs.rust-jemalloc-sys-unprefixed
           self'.packages.rocksdb
+          pkgs.rust-jemalloc-sys-unprefixed
+          pkgs.nodejs
+          pkgs.liburing
+          pkgs.pkg-config
         ];
-        env.LIBCLANG_PATH = lib.makeLibraryPath [ pkgs.llvmPackages.libclang.lib ];
+
+        env = {
+          LIBCLANG_PATH = lib.makeLibraryPath [ pkgs.llvmPackages.libclang.lib ];
+          LD_LIBRARY_PATH = lib.makeLibraryPath [
+            pkgs.liburing
+            pkgs.jemalloc
+            pkgs.stdenv.cc.cc.lib
+          ];
+          PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" [
+            pkgs.liburing.dev
+          ];
+        };
       };
     };
 }
