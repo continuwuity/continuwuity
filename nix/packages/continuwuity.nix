42028f155b2717531eecb272d225e26fff395abd
aviac <aviac@mailbox.org>
2026-04-13 15:40:51 +0200

    feat(nix): also add release-max-perf package build to flake outputs

--- nix/packages/continuwuity.nix
+++ nix/packages/continuwuity.nix
@@ -9,6 +9,7 @@
   rustPlatform,
   cargoExtraArgs ? "",
   rocksdb ? callPackage ./rocksdb.nix { },
+  profile ? "release",
 }:
 let
   # see https://crane.dev/API.html#cranelibfiltercargosources
@@ -35,6 +36,7 @@ let
     env = {
       ROCKSDB_INCLUDE_DIR = "${rocksdb}/include";
       ROCKSDB_LIB_DIR = "${rocksdb}/lib";
+      CARGO_PROFILE = profile;
     };
   };
 in
--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -5,6 +5,7 @@
 {
   perSystem =
     {
+      self',
       pkgs,
       craneLib,
       ...
@@ -13,6 +14,14 @@
       packages = {
         rocksdb = pkgs.callPackage ./rocksdb.nix { };
         default = pkgs.callPackage ./continuwuity.nix { inherit self craneLib; };
+        # users may also override this with other cargo profiles to build for other feature sets
+        #
+        # other examples include:
+        #
+        # - release-high-perf
+        max-perf = self'.packages.default.override {
+          profile = "release-max-perf";
+        };
       };
     };
 }
