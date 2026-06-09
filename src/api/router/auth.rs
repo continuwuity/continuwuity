61a2e236b6474dda2169d7921fc66c8c09e2a83d
Ginger <ginger@gingershaped.computer>
2026-06-09 14:45:20 -0400

    fix: Fix backwards logic in auth check

--- src/api/router/auth.rs
+++ src/api/router/auth.rs
@@ -314,7 +314,7 @@ async fn verify_access_token(
 		{
 			if required_scopes
 				.iter()
-				.any(|scope| session.scopes.contains(scope))
+				.all(|scope| !session.scopes.contains(scope))
 			{
 				return Err!(Request(Forbidden(
 					"You don't have the necessary scopes to use this endpoint."
@@ -323,7 +323,7 @@ async fn verify_access_token(
 		} else {
 			// Otherwise, explicitly check if the endpoint is restricted to admins only.
 			if required_scopes.contains(&OAuthScope::ServerAdministration)
-				&& services.users.is_admin(&sender_user).await
+				&& !services.users.is_admin(&sender_user).await
 			{
 				return Err!(Request(Forbidden(
 					"Only server administrators can use this endpoint."
