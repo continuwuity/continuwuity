e7b302955f4c4e3329f6d91c103b69ef58256619
timedout <git@nexy7574.co.uk>
2026-06-26 06:29:53 +0100

    style: Eliminate `implement` database

--- src/database/map/remove.rs
+++ src/database/map/remove.rs
@@ -1,55 +1,53 @@
 use std::{convert::AsRef, fmt::Debug, io::Write};
 
-use conduwuit::{arrayvec::ArrayVec, implement};
+use conduwuit::arrayvec::ArrayVec;
 use serde::Serialize;
 
 use crate::{keyval::KeyBuf, ser, util::or_else};
 
-#[implement(super::Map)]
-#[inline]
-pub fn del<K>(&self, key: K)
-where
-	K: Serialize + Debug,
-{
-	let mut buf = KeyBuf::new();
-	self.bdel(key, &mut buf);
-}
+impl super::Map {
+	#[inline]
+	pub fn del<K>(&self, key: K)
+	where
+		K: Serialize + Debug,
+	{
+		let mut buf = KeyBuf::new();
+		self.bdel(key, &mut buf);
+	}
 
-#[implement(super::Map)]
-#[inline]
-pub fn adel<const MAX: usize, K>(&self, key: K)
-where
-	K: Serialize + Debug,
-{
-	let mut buf = ArrayVec::<u8, MAX>::new();
-	self.bdel(key, &mut buf);
-}
+	#[inline]
+	pub fn adel<const MAX: usize, K>(&self, key: K)
+	where
+		K: Serialize + Debug,
+	{
+		let mut buf = ArrayVec::<u8, MAX>::new();
+		self.bdel(key, &mut buf);
+	}
 
-#[implement(super::Map)]
-#[tracing::instrument(skip(self, buf), level = "trace")]
-pub fn bdel<K, B>(&self, key: K, buf: &mut B)
-where
-	K: Serialize + Debug,
-	B: Write + AsRef<[u8]>,
-{
-	let key = ser::serialize(buf, key).expect("failed to serialize deletion key");
-	self.remove(key);
-}
+	#[tracing::instrument(skip(self, buf), level = "trace")]
+	pub fn bdel<K, B>(&self, key: K, buf: &mut B)
+	where
+		K: Serialize + Debug,
+		B: Write + AsRef<[u8]>,
+	{
+		let key = ser::serialize(buf, key).expect("failed to serialize deletion key");
+		self.remove(key);
+	}
 
-#[implement(super::Map)]
-#[tracing::instrument(skip(self, key), fields(%self), level = "trace")]
-pub fn remove<K>(&self, key: &K)
-where
-	K: AsRef<[u8]> + ?Sized + Debug,
-{
-	let write_options = &self.write_options;
-	self.db
-		.db
-		.delete_cf_opt(&self.cf(), key, write_options)
-		.or_else(or_else)
-		.expect("database remove error");
+	#[tracing::instrument(skip(self, key), fields(%self), level = "trace")]
+	pub fn remove<K>(&self, key: &K)
+	where
+		K: AsRef<[u8]> + ?Sized + Debug,
+	{
+		let write_options = &self.write_options;
+		self.db
+			.db
+			.delete_cf_opt(&self.cf(), key, write_options)
+			.or_else(or_else)
+			.expect("database remove error");
 
-	if !self.db.corked() {
-		self.db.flush().expect("database flush error");
+		if !self.db.corked() {
+			self.db.flush().expect("database flush error");
+		}
 	}
 }
